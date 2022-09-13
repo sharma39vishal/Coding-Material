8d2c197092c672e3d75be138a7eac23433f68a21
Vishal Sharma <85349991+sharma39vishal@users.noreply.github.com>
2022-09-13 16:01:42 +0530

    Create Code.cpp

---
+++ Code.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void fileio(){
+
+#ifndef ONLINE_JUDGE
+
+    freopen("input.txt", "r", stdin);
+
+    freopen("output.txt", "w", stdout);
+
+    freopen("Error.txt", "w", stderr);
+
+#endif
+
+}
+int main(){
+
+fileio();
+  }
